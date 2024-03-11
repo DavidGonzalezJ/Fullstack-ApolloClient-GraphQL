f1993939f89d8813776fc08a9680389821ee4484
David <davidgonzalezj97@gmail.com>
2024-03-11 15:46:18 +0100

    Exercise 8.11 Adding functionality to update the birth year of existent authors

--- src/components/Authors.js
+++ src/components/Authors.js
@@ -1,3 +1,53 @@
+import { UPDATE_BIRTHYEAR, ALL_AUTHORS } from "../queries"
+import { useState } from "react"
+import { useMutation } from "@apollo/client"
+
+const BirthForm = () => {
+  const [name, setName] = useState('')
+  const [birth, setBirth] = useState('')
+
+  const [updateAuthor] = useMutation(UPDATE_BIRTHYEAR, {
+    refetchQueries: [{query:ALL_AUTHORS}],
+    onError: (error) => {
+      const messages = error.graphQLErrors.map(e => e.message).join('\n')
+      console.log(messages)
+      console.log('WUT')
+    }
+  })
+
+  const submit = (event) => {
+    event.preventDefault()
+    const setBornTo = parseInt(birth)
+    updateAuthor({ variables: {name, setBornTo}})
+
+    setName('')
+    setBirth('')
+  }
+
+  return (
+    <>
+      <h3>Set birthyear</h3>
+      <form onSubmit={submit}>
+        <div>
+          name
+          <input
+            value={name}
+            onChange={({ target }) => setName(target.value)}
+          />
+        </div>
+        <div>
+          born
+          <input
+            value={birth}
+            onChange={({ target }) => setBirth(target.value)}
+          />
+        </div>
+        <button type="submit">update author</button>
+      </form>
+    </>
+  )
+}
+
 const Authors = ({list}) => {
   const authors = list
 
@@ -20,6 +70,8 @@ const Authors = ({list}) => {
           ))}
         </tbody>
       </table>
+      <BirthForm />
+
     </div>
   )
 }
--- src/queries.js
+++ src/queries.js
@@ -34,3 +34,15 @@ export const CREATE_BOOK = gql`
     }
   }
 `
+
+export const UPDATE_BIRTHYEAR = gql`
+  mutation editBirth($name: String!, $setBornTo: Int!){
+    editAuthor(
+        name: $name
+        setBornTo: $setBornTo
+    ) {
+        name
+        born
+    }
+  }
+`
